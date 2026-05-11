d0e14d783b8802de5b2da5b1d2ac5f2cf86522bf
Neelthakkar18 <tneel9414@gmail.com>
2026-05-11 12:03:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -279,7 +279,11 @@ def handle_delete_message(data):
 
 if __name__ == "__main__":
 
+    port = int(os.environ.get("PORT", 10000))
+
     socketio.run(
         app,
-        debug=True
+        host="0.0.0.0",
+        port=port,
+        debug=False
     )
